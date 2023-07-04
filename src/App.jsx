c311d18a78dfe1bd34dc80c9f202b8fccce6b8bd
Milan Kocic <comy.87@gmail.com>
2023-07-04 20:56:12 +0200

    pocetak projekta

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product.jsx';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
+import ErrorPage from './pages/ErrorPage.jsx';
+import AppLayout from './pages/AppLayout.jsx';
+import Login from './pages/Login.jsx';
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
+        <Route path='app' element={<AppLayout />} />
+
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageNav from '../components/PageNav';
+
+export default function ErrorPage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>PAGE NOT FOUND</h1>
+    </div>
+  );
+}
