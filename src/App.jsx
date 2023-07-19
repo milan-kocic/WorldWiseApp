b03473da1fa573ce8f10396146984da76d8ecfa5
Milan Kocic <comy.87@gmail.com>
2023-07-19 16:37:30 +0200

    authentificationProtecting

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import City from './components/City.jsx';
 import Form from './components/Form.jsx';
 import { CitiesProvider } from './context/CitiesContext.jsx';
 import { AuthProvider } from './context/FakeAuthContext.jsx';
+import ProtectedRoute from './pages/ProtectedRoute.jsx';
 function App() {
   return (
     <AuthProvider>
@@ -21,7 +22,14 @@ function App() {
             <Route path='product' element={<Product />} />
             <Route path='pricing' element={<Pricing />} />
             <Route path='login' element={<Login />} />
-            <Route path='app' element={<AppLayout />}>
+            <Route
+              path='app'
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to='cities' />} />
               <Route path='cities' element={<CityList />} />
               <Route path='cities/:id' element={<City />} />
